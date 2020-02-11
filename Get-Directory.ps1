36e2f0cc69854f9f7d93f0e24d2fd045a071975e
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2020-02-11 14:00:02 +0000

    Update Get-Directory.ps1

--- Get-Directory.ps1
+++ Get-Directory.ps1
@@ -1,22 +1,32 @@
 <#
     Script: Get-Directory.ps1
     Author: Dark-Coffee
-    Version: 1.0
-    Updated: 2020-02-05
+    Version: 2.0
+    Updated: 2020-02-11
     Description: dir, but directory listings only, and it actually shows folder sizes! :O 
 #>
 
 function Get-Directory {
     param (
-        [Parameter(Mandatory=$false)][string]$Path = $PWD
+        [Parameter(Mandatory=$false)][string]$Path = $PWD,
+        [Parameter(Mandatory=$false)][string]$SizeIn = 'MB'
     )
 
+    #Param Validation
+    if('KB','MB','GB' -notcontains $SizeIn){Write-Error -Category InvalidArgument -Message 'SizeIn must be in: KB | MB | GB'; exit;}else{
+
+    #Sort Sizes
+    $SizeIn = $SizeIn.ToUpper()
+    $SizeMeasure = "1$SizeIn"
+
     #Declare Expression
-    $Size = @{n='Size (MB)';e={Get-ChildItem $_ -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Sum | ForEach-Object {[Math]::Round(($_ / 1MB),2)}}} 
+    $Size = @{n="Size ($SizeIn)";e={Get-ChildItem $_ -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Sum | ForEach-Object {[Math]::Round(($_ / $SizeMeasure),2)}}}
 
     #Logic
     $Directories = Get-ChildItem $Path -Directory
     $DirectorySizes = Foreach($Directory in $Directories){$Directory | Select-Object Mode, LastWriteTime, $Size, Name}
 
+    }
+    #Return
     $DirectorySizes
 }
