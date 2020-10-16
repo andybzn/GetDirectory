71ae1095372492ab15840e762f96e989e4390371
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2020-10-16 10:35:46 +0100

    Remove ModuleManifest, Update Help

--- GetDirectory.psm1
+++ GetDirectory.psm1
@@ -1,7 +1,7 @@
 <#
     Module: GetDirectory.psm1
     Author: Dark-Coffee
-    Version: 3.1
+    Version: 3.2
     Updated: 2020-10-15
     Description: dir, but it actually shows folder sizes! :O 
 #>
@@ -11,6 +11,30 @@ function Get-Directory {
         [Parameter(Mandatory=$false)][string]$Path = $PWD,
         [Parameter(Mandatory=$false)][string]$SizeIn = 'MB'
     )
+
+    #Param Validation
+    if('KB','MB','GB' -notcontains $SizeIn){Write-Error -Category InvalidArgument -Message 'SizeIn must be in: KB | MB | GB'}else{
+
+    #Sort Sizes
+    $SizeIn = $SizeIn.ToUpper()
+    $SizeMeasure = "1$SizeIn"
+
+    #Declare Expression
+    $Size = @{n="Size ($SizeIn)";e={$_ | Get-ChildItem -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Sum | ForEach-Object {[Math]::Round(($_ / $SizeMeasure),2)}}}
+    $Size2 = @{n="Size ($SizeIn)";e={$_ | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Sum | ForEach-Object {[Math]::Round(($_ / $SizeMeasure),2)}}}
+
+    #Directory Logic
+    $Directories = Get-ChildItem -Path $Path -Directory
+    $DirectoryOutput = Foreach($Directory in $Directories){$Directory | Select-Object Mode, LastWriteTime, $Size, Name | Sort-Object Name}
+
+    #Item Logic
+    $Files = Get-ChildItem -Path $Path -File
+    $FileOutput = Foreach($File in $Files){$File | Select-Object Mode, LastWriteTime, $Size, Name | Sort-Object Name}
+    }
+    
+    $DirectoryListing = $DirectoryOutput,$FileOutput
+    #Return
+    $DirectoryListing
 
     <#
     .SYNOPSIS
@@ -61,31 +85,6 @@ function Get-Directory {
     -a---- 10/09/2020 08:05:38      0.41 steam.dll
 
     #>
-
-
-    #Param Validation
-    if('KB','MB','GB' -notcontains $SizeIn){Write-Error -Category InvalidArgument -Message 'SizeIn must be in: KB | MB | GB'}else{
-
-    #Sort Sizes
-    $SizeIn = $SizeIn.ToUpper()
-    $SizeMeasure = "1$SizeIn"
-
-    #Declare Expression
-    $Size = @{n="Size ($SizeIn)";e={$_ | Get-ChildItem -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Sum | ForEach-Object {[Math]::Round(($_ / $SizeMeasure),2)}}}
-    $Size2 = @{n="Size ($SizeIn)";e={$_ | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Sum | ForEach-Object {[Math]::Round(($_ / $SizeMeasure),2)}}}
-
-    #Directory Logic
-    $Directories = Get-ChildItem -Path $Path -Directory
-    $DirectoryOutput = Foreach($Directory in $Directories){$Directory | Select-Object Mode, LastWriteTime, $Size, Name | Sort-Object Name}
-
-    #Item Logic
-    $Files = Get-ChildItem -Path $Path -File
-    $FileOutput = Foreach($File in $Files){$File | Select-Object Mode, LastWriteTime, $Size, Name | Sort-Object Name}
-    }
-    
-    $DirectoryListing = $DirectoryOutput,$FileOutput
-    #Return
-    $DirectoryListing
 }
 
 Export-ModuleMember -Function Get-Directory
